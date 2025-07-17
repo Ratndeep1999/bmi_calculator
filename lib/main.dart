ad05e621fa09454a22a6ba67cbd0c842d5acbce2
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-17 11:42:04 +0530

    bmi_calculator_page.dart : page add

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'bmi_calculator_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(
               seedColor: Colors.white,
@@ -24,102 +26,4 @@ class MyApp extends StatelessWidget {
 }
 
 
-class BmiCalculatorPage extends StatefulWidget {
-  const BmiCalculatorPage({super.key});
 
-  @override
-  State<BmiCalculatorPage> createState() {
-    return _BmiCalculatorPageState();
-  }
-}
-
-class _BmiCalculatorPageState extends State<BmiCalculatorPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.menu),
-        centerTitle: true,
-        title: Text("Counter App", style: TextStyle()),
-        backgroundColor: Colors.orange.shade100,
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15),
-          child: Column(
-            children: [
-              SizedBox(height: 15),
-
-              // Gender Section
-              Row(
-                children: [
-                  // Male container
-                  Expanded(
-                    child: Container(
-                      height: MediaQuery.of(context).size.height * 0.20,
-                      width: MediaQuery.of(context).size.width * 0.10,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.grey.shade300,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.male,
-                            size: 100,
-                            color: Colors.blue.shade100,
-                          ),
-                          Text(
-                            "Male",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w800,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(width: 15),
-
-                  // Female container
-                  Expanded(
-                    child: Container(
-                      height: MediaQuery.of(context).size.height * 0.20,
-                      width: MediaQuery.of(context).size.width * 0.10,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.grey.shade300,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.female,
-                            size: 100,
-                            color: Colors.pink.shade100,
-                          ),
-                          Text(
-                            "Female",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w800,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
